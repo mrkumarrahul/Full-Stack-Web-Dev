061e36a9f8da343662568f2b0d19e849296b271b
mrkumarrahul <Kumarrahulmahto1211@gmail.com>
2023-11-05 03:51:42 +0530

    creating react components

---
+++ REACT/2-create-react-comp/create-first-comp/src/App.jsx
@@ -0,0 +1,15 @@
+import KgButton from "./KgButton";
+import Hello from "./Hello";
+import Random from "./Random";
+function App() {
+  return <div>
+    <h1 style={{backgroundColor:'blue'}}>Hello World</h1>
+    <Hello></Hello>
+    <Random></Random>
+    <Random></Random>
+    <Random></Random>
+    <Random></Random>
+    <Random></Random>
+        </div>
+}
+export default App;
---
+++ REACT/2-create-react-comp/create-first-comp/src/Hello.jsx
@@ -0,0 +1,9 @@
+function Hello() {
+  let name='Prasant'
+  let fullName=()=> {
+    return 'Prasant Jain'
+  }
+  let number='124'
+  return <h3>Hello this is the future speaking.I am your instructor {fullName( )}.you can Contact me on this number:{number} </h3>
+}
+export default Hello;
