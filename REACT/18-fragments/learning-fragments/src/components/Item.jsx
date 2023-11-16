ffb4b70fa9e8991cb3016be0303a20b84f07cc8f
mrkumarrahul <Kumarrahulmahto1211@gmail.com>
2023-11-16 08:16:45 +0530

    Css

--- REACT/18-fragments/learning-fragments/src/components/Item.jsx
+++ REACT/18-fragments/learning-fragments/src/components/Item.jsx
@@ -1,5 +1,7 @@
+import styles from "./item.module.css"
+
 const Item= ({foodItem}) => {
   // let {foodItem}=props;
-  return <li  className="list-group-item kg-item" ><span className="kg-span">{foodItem}</span> </li>
+  return <li  className={`${styles["kg-item" ]}`} ><span className={`${styles["kg-span"]}`}> {foodItem}</span> </li>
 };
 export default Item;
